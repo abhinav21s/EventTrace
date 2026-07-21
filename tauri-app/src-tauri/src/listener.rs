0db6b133ca7c31b6186ed05b762c8a791cbbae20
Abhinav <abhinav.sharma01030@gmail.com>
2026-07-21 11:06:09 +0530

    added session analytics

--- tauri-app/src-tauri/src/listener.rs
+++ tauri-app/src-tauri/src/listener.rs
@@ -3,12 +3,21 @@ use crate::logger::file_creation_and_write;
 use crate::logger::create_filename;
 use std::sync::Mutex;
 use std::sync::OnceLock;
+use std::sync::atomic::{AtomicI32,Ordering};
 #[derive(Debug)]
  enum Loggedstate{
    Paused,
    Logging,
    Stoped
 }
+
+static Counter_key:AtomicI32=AtomicI32::new(0);
+static Counter_mouse_button_left:AtomicI32=AtomicI32::new(0);
+static Counter_mouse_button_right:AtomicI32=AtomicI32::new(0);
+static Counter_mouse_button_middle:AtomicI32=AtomicI32::new(0);
+static Counter_mouse_move:AtomicI32=AtomicI32::new(0);
+static Counter_wheel:AtomicI32=AtomicI32::new(0);
+static Session_count:AtomicI32=AtomicI32::new(1);
 static CURRENT_STATE:OnceLock<Mutex<Loggedstate>>=OnceLock::new();
 pub fn init_state(){
     
@@ -47,9 +56,34 @@ match *state{
 }
 }
 
+pub fn display_count(){
+    let session_value=Session_count.load(Ordering::SeqCst);
+    println!("{}",session_value);
+    let value_k=Counter_key.load(Ordering::SeqCst);
+    println!("{}",value_k);
+    let value_mouse_m_l=Counter_mouse_button_left.load(Ordering::SeqCst);
+    println!("{}",value_mouse_m_l);
+    let value_mouse_m_r=Counter_mouse_button_right.load(Ordering::SeqCst);
+    println!("{}",value_mouse_m_r);
+    let value_mouse_m_m=Counter_mouse_button_middle.load(Ordering::SeqCst);
+    println!("{}",value_mouse_m_m);
+    let value_wheel=Counter_wheel.load(Ordering::SeqCst);
+    println!("{}",value_wheel);
+    let value_mouse_mov=Counter_mouse_move.load(Ordering::SeqCst);
+    println!("{}",value_mouse_mov);
+}
+
+pub fn change_counter(){
+    Counter_key.store(0,Ordering::SeqCst);
+    Counter_mouse_button_left.store(0,Ordering::SeqCst);
+    Counter_mouse_button_right.store(0,Ordering::SeqCst);
+    Counter_mouse_button_middle.store(0,Ordering::SeqCst);
+    Counter_mouse_move.store(0,Ordering::SeqCst);
+    Counter_wheel.store(0,Ordering::SeqCst);
+}
 pub fn stop_logging() {
     let mut state = CURRENT_STATE.get().unwrap().lock().unwrap();
-
+    display_count();
     match *state {
         Loggedstate::Stoped => {
             println!("Already stopped");
@@ -62,6 +96,9 @@ pub fn stop_logging() {
 }
 
 pub fn new_session(folderPath:String){
+    change_counter();
+    Session_count.fetch_add(1,Ordering::SeqCst);
+    
 {
         let mut state = CURRENT_STATE.get().unwrap().lock().unwrap();
 
@@ -73,7 +110,7 @@ pub fn new_session(folderPath:String){
             _ => {}
         }
     }
-
+   
     create_filename(folderPath);
 
     {
@@ -97,6 +134,7 @@ pub fn callback(event:Event){
     println!("{:?}", event.event_type);
     match event.event_type{
         EventType::KeyPress(key)=>{
+         Counter_key.fetch_add(1,Ordering::SeqCst);
        let msg=format!("Key pressed is {:?} at time {:?}\n",key,event.time);
        file_creation_and_write(msg);
     }
@@ -105,14 +143,17 @@ pub fn callback(event:Event){
         file_creation_and_write(msg);
     }
     EventType::ButtonPress(Button::Left)=>{
+        Counter_mouse_button_left.fetch_add(1,Ordering::SeqCst);
        let msg=format!("Button Pressed is {:?} at time {:?}\n",Button::Left,event.time);
        file_creation_and_write(msg);
     }
     EventType::ButtonPress(Button::Right)=>{
+         Counter_mouse_button_right.fetch_add(1,Ordering::SeqCst);
         let msg=format!("Button Pressed is {:?} at time {:?}\n",Button::Right,event.time);
         file_creation_and_write(msg);
     }
     EventType::ButtonPress(Button::Middle)=>{
+          Counter_mouse_button_middle.fetch_add(1,Ordering::SeqCst);
         let msg=format!("Button Pressed is {:?} at time {:?}\n",Button::Middle,event.time);
         file_creation_and_write(msg);
     }
@@ -129,14 +170,17 @@ pub fn callback(event:Event){
          file_creation_and_write(msg);
     }
     EventType::MouseMove { x, y }=>{
+        Counter_mouse_move.fetch_add(1,Ordering::SeqCst);
       let msg=format!("Mouse moved {} {}\n",x,y);
       file_creation_and_write(msg);
     }
     EventType::Wheel { delta_x, delta_y }=>{
+        Counter_wheel.fetch_add(1,Ordering::SeqCst);
         let msg=format!("Wheel scrolled at {} {}\n",delta_x,delta_y);
         file_creation_and_write(msg);
     }
     _=>{
+      
         let msg=String::from("Unknown key");
         file_creation_and_write(msg);
     }
